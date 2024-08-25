554eac230eb9b5664e01c651361c5a0ae4255475
sanketbanket <www.sanketramteke13@gmail.com>
2024-08-25 12:53:33 +0530

    added theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,18 @@
+import 'package:anonwork/post_work.dart';
 import 'package:flutter/material.dart';
+import 'post_work.dart';
 import 'mainscreen.dart';
 
-final myApp=MaterialApp(
+final myApp = MaterialApp(
   initialRoute: '/',
-  routes: {
-    '/': (context) => MainScreen()
-  },
+  routes: {'/': (context) => PostScreen()},
+  theme: ThemeData(
+    primaryColor: Colors.white60,
+    brightness: Brightness.dark,
+    fontFamily: 'JosefinSans',
+  ),
 );
 
 void main() {
   runApp(myApp);
 }
-
