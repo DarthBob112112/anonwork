ee8ea13d5dd41e17716500730dcd3392310702cc
sanketbanket <www.sanketramteke13@gmail.com>
2024-08-25 14:47:51 +0530

    added text controllers

--- lib/post_work.dart
+++ lib/post_work.dart
@@ -1,28 +1,51 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-class PostScreen extends StatelessWidget {
+class PostScreen extends StatefulWidget {
   const PostScreen({super.key});
+
+  @override
+  State<PostScreen> createState() => _PostScreenState();
+}
+
+class _PostScreenState extends State<PostScreen> {
+  final TextEditingController _controller1 =
+      TextEditingController(); //service description
+  final TextEditingController _controller2 = TextEditingController(); //category
+  final TextEditingController _controller3 =
+      TextEditingController(); //delivery time
+  @override
+  void dispose() {
+    _controller1.dispose();
+    _controller2.dispose();
+    _controller3.dispose();
+
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     //ThemeData theme = Theme.of(context);
-    return const Scaffold(
+    return Scaffold(
       body: Padding(
         padding: EdgeInsets.all(50.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Text(
+            IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.home)), //no home navigation yet
+            const Text(
               "What service would you like?",
               style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
             ),
             customTextField(
               name: 'Enter your service description',
               icon: Icon(Icons.shop),
+              controller: _controller1,
             ),
-            Text(
+            const Text(
               "Enter a category : ",
               style: TextStyle(
                 fontWeight: FontWeight.bold,
@@ -32,16 +55,27 @@ class PostScreen extends StatelessWidget {
             customTextField(
               name: 'Category',
               icon: Icon(Icons.check_box),
+              controller: _controller2,
             ),
-            Text(
+            const Text(
               "What is your expected delivery time?",
               style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
             ),
             customTextField(
               name: 'DAYS : HOURS : MINUTES : SECONDS',
               icon: Icon(Icons.punch_clock),
+              controller: _controller3,
             ),
-            customButton(name: "Post"),
+            ElevatedButton(
+              onPressed: () {
+                String x = _controller1.text;
+                print('$x'); // ADD A ON PRESSED FUNCTION
+              },
+              child: const Text(
+                "Bruh",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            ),
           ],
         ),
       ),
@@ -52,37 +86,22 @@ class PostScreen extends StatelessWidget {
 class customTextField extends StatelessWidget {
   final String name;
   final Icon icon;
-  //TextEditingController controller;
+  final TextEditingController controller;
   const customTextField({
     super.key,
     required this.name,
     required this.icon,
-    //required this.controller,
+    required this.controller,
   });
   @override
   Widget build(BuildContext context) {
     return TextField(
+      controller: controller,
       decoration: InputDecoration(
         border: OutlineInputBorder(),
         labelText: name,
         icon: icon,
       ),
     );
-  }
-}
-
-class customButton extends StatelessWidget {
-  final String name;
-
-  const customButton({super.key, required this.name});
-
-  @override
-  Widget build(BuildContext context) {
-    return ElevatedButton(
-        onPressed: () {},
-        child: Text(
-          name,
-          style: TextStyle(fontWeight: FontWeight.bold),
-        ));
   }
 }
