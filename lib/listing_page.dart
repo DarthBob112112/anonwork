8d72303958a0723aedb9a7c210742ea2b130361c
sanketbanket <www.sanketramteke13@gmail.com>
2024-08-25 15:35:04 +0530

    added a place where jobs are shown

---
+++ lib/listing_page.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class ListingPage extends StatefulWidget {
+  const ListingPage({super.key});
+
+  @override
+  State<ListingPage> createState() => _ListingPageState();
+}
+
+void addJob(List<List<String>> lis, String dscript, String category,
+    String time, String amount) {
+  lis.add([dscript, category, time, amount]);
+}
+
+class _ListingPageState extends State<ListingPage> {
+  final List<List<String>> x = [];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListBody(
+        children: [for (int i = 0; i < x.length; i++) JobListCard(job: x[i])],
+      ),
+    );
+  }
+}
+
+class JobListCard extends StatelessWidget {
+  final List<String> job;
+  const JobListCard({super.key, required this.job});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Column(
+        children: [
+          Text(job[0]),
+          Text(job[1]),
+          Text(job[2]),
+          Text(job[3]),
+        ],
+      ),
+    );
+  }
+}
